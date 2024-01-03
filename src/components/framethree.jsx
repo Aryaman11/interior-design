b0285f2765bacfd1020548cbf0faa239023333d7
Adamya Singh <adamyasingh22@gmail.com>
2024-01-03 15:42:34 +0530

    created framethree

---
+++ src/components/framethree.jsx
@@ -0,0 +1,21 @@
+function Framethree (){
+    return(
+        <div className="inline-flex mt-14">
+            <div className="p-12">
+             <img src='./../../public/image/Vector.png'/>
+            </div>
+            <div className="p-12">
+             <img src='./../../public/image/big.png'/> 
+            </div>
+            <div className="p-12">
+             <img src='./../../public/image/eon.png'/>
+            </div>
+            <div className="p-12">
+             <img src='./../../public/image/Frame.png'/>
+            </div> 
+          
+           
+        </div>
+    )
+}
+export default Framethree;
