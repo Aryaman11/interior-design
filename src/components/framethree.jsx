1ade84098681245fdf4d354b9d77186570321d13
Adamya Singh <adamyasingh22@gmail.com>
2024-01-03 16:15:08 +0530

    framethree modified

--- src/components/framethree.jsx
+++ src/components/framethree.jsx
@@ -1,20 +1,21 @@
+import Vector from './../../public/image/Vector.png'
+import Vector2 from './../../public/image/big.png'
+import Vector3 from './../../public/image/eon.png'
+import Vector4 from './../../public/image/Frame.png'
+
 function Framethree (){
+    const images = [Vector, Vector2, Vector3, Vector4];
     return(
         <div className="inline-flex mt-14">
-            <div className="p-12">
-             <img src='./../../public/image/Vector.png'/>
-            </div>
-            <div className="p-12">
-             <img src='./../../public/image/big.png'/> 
-            </div>
-            <div className="p-12">
-             <img src='./../../public/image/eon.png'/>
-            </div>
-            <div className="p-12">
-             <img src='./../../public/image/Frame.png'/>
-            </div> 
-          
-           
+            {
+                images.map((item, i) => {
+                    return(
+                        <div key={i} className="p-12">
+                            <img src={item}/>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
