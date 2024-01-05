bc946ed6bc34be317a88c4ddf2626dceb0cfa6f3
Adamya Singh <adamyasingh22@gmail.com>
2024-01-06 02:08:35 +0530

    framesix

--- src/components/framesix.jsx
+++ src/components/framesix.jsx
@@ -1,19 +1,19 @@
 function Framesix(){
     return(
         <div className="mt-16 flex items-center justify-center">
-            <div className="self-center border-r-2 pr-px">
+            <div className="self-center border-r-2 pr-14">
                 <p className="text-7xl text-center text-yellow-600 font-serif">12</p>
                 <p>Years of expiernce</p>
             </div>
-            <div className="justify-start border-r-2 pr-px">
+            <div className="justify-start border-r-2 px-14">
                 <p className="text-7xl text-center text-yellow-600 font-serif">1074</p>
                 <p className="text-center">success projects</p>
             </div>
-            <div className="justify-start border-r-2 pr-px">
+            <div className="justify-start border-r-2 px-14">
                 <p className="text-7xl text-center text-yellow-600 font-serif">98</p>
                 <p className="text-center">Active Projects</p>
             </div>
-            <div className="justify-start">
+            <div className="justify-start pl-14">
                 <p className="text-7xl text-center text-yellow-600 font-serif">583</p>
                 <p>happy customers</p>
             </div>
