900495a689c77ceedf954802480f25408c20d28d
Adamya Singh <adamyasingh22@gmail.com>
2024-01-09 22:49:36 +0530

    projects

---
+++ src/pages/projects.jsx
@@ -0,0 +1,11 @@
+import Footer from "../components/footer";
+import Header from "../components/header";
+function Projects () {
+     return(
+        <div>
+            <header/>
+            <footer/>
+
+        </div>
+     )
+}
