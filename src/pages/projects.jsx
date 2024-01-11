cdbf2b06010202bcc1fa8cc7f6b1d0ce5fb04017
Adamya Singh <adamyasingh22@gmail.com>
2024-01-11 20:29:03 +0530

    project page final

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -7,10 +7,135 @@ function Projects () {
         <div>
            <Header/> 
            <ListingBanner info={"Our Project"}/>
-           <div className='flex mt-32 mb-44 mx-40'>
-             <div className="flex mx-auto">
-                <p className="text-2xl border-x-8 border-black rounded-md">Bedroom</p>
+           <div className='flex mt-32 mb-44 mx-40 mt-6'>
+             <div className="flex mx-auto bg-orange-400 rounded-lg">
+                <p className="text-2xl px-px ">Bedroom</p>
              </div>
+           </div>
+           <div className="mt-32 mb-44 mx-40 mt-6">
+            <div className="grid grid-cols-2 gap-y-px mt-28">
+             <div className="mx-8 my-8">
+              <img src="./../../public/image/Rectangle27.png"/>
+              <div className="flex  space-x-72 mt-3">
+                <div>
+                 <p className="text-xl">Modern Bedroom</p>
+                 <p>Decor/Artchitech</p>
+                </div>
+                <div>
+                 <svg width="60" height="60" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+                 <g id="&#62;">
+                 <circle id="Ellipse" cx="35" cy="35" r="35" fill="#D8A876"/>
+                 <g id="Vector">
+                 <path d="M32 44L40 35L32 26" fill="#D8A876"/>
+                 <path d="M32 44L40 35L32 26" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </g>
+                </g>
+               </svg>
+               </div>
+              </div>
+            </div>
+            <div className="mx-8 my-8">
+              <img src="./../../public/image/Rectangle28.png"/>
+              <div className="flex  space-x-72 mt-3">
+              <div>
+                <p className="text-xl">Modern Bedroom</p>
+                <p>Decor/Artchitech</p>
+              </div>
+              <div>
+               <svg width="60" height="60" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+               <g id="&#62;">
+               <circle id="Ellipse" cx="35" cy="35" r="35" fill="#D8A876"/>
+               <g id="Vector">
+               <path d="M32 44L40 35L32 26" fill="#D8A876"/>
+               <path d="M32 44L40 35L32 26" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+               </g>
+               </g>
+               </svg>
+              </div>
+              </div>
+            </div>
+            <div className="mx-8 my-8">
+              <img src="./../../public/image/Rectangle29.png"/>
+              <div className="flex  space-x-72 mt-3">
+              <div>
+                <p className="text-xl">Modern Bedroom</p>
+                <p>Decor/Artchitech</p>
+              </div>
+              <div>
+               <svg width="60" height="60" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+               <g id="&#62;">
+               <circle id="Ellipse" cx="35" cy="35" r="35" fill="#D8A876"/>
+               <g id="Vector">
+               <path d="M32 44L40 35L32 26" fill="#D8A876"/>
+               <path d="M32 44L40 35L32 26" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+               </g>
+               </g>
+               </svg>
+              </div>
+              </div>
+            </div>
+            <div className="mx-8 my-8">
+              <img src="./../../public/image/Rectangle30.png"/>
+              <div className="flex  space-x-72 mt-3">
+              <div>
+                <p className="text-xl">Modern Bedroom</p>
+                <p>Decor/Artchitech</p>
+              </div>
+              <div>
+               <svg width="60" height="60" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+               <g id="&#62;">
+               <circle id="Ellipse" cx="35" cy="35" r="35" fill="#D8A876"/>
+               <g id="Vector">
+               <path d="M32 44L40 35L32 26" fill="#D8A876"/>
+               <path d="M32 44L40 35L32 26" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+               </g>
+               </g>
+               </svg>
+              </div>
+              </div>
+            </div>
+            <div className="mx-8 my-8">
+              <img src="./../../public/image/Rectangle31.png"/>
+              <div className="flex  space-x-72 mt-3">
+              <div>
+                <p className="text-xl">Modern Bedroom</p>
+                <p>Decor/Artchitech</p>
+              </div>
+              <div>
+               <svg width="60" height="60" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+               <g id="&#62;">
+               <circle id="Ellipse" cx="35" cy="35" r="35" fill="#D8A876"/>
+               <g id="Vector">
+               <path d="M32 44L40 35L32 26" fill="#D8A876"/>
+               <path d="M32 44L40 35L32 26" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+               </g>
+               </g>
+               </svg>
+              </div>
+              </div>
+            </div>
+            <div className="mx-8 my-8">
+              <img src="./../../public/image/Rectangle32.png"/>
+              <div className="flex  space-x-72 mt-3">
+              <div>
+                <p className="text-xl">Modern Bedroom</p>
+                <p>Decor/Artchitech</p>
+              </div>
+              <div>
+               <svg  width="60" height="60" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+               <g id="&#62;">
+               <circle id="Ellipse" cx="35" cy="35" r="35" fill="#D8A876"/>
+               <g id="Vector">
+               <path d="M32 44L40 35L32 26" fill="#D8A876"/>
+               <path d="M32 44L40 35L32 26" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+               </g>
+               </g>
+               </svg>
+              </div>
+              </div>
+            </div>
+
+            </div>
            </div>
            <Footer/>
         </div>
